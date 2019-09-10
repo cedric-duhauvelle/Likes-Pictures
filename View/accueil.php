3f26588a7a850ff431a10acee41a25672f1bcb73
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-09-10 09:31:14 +0200

    Mise en page de la page accueil

--- View/accueil.php
+++ View/accueil.php
@@ -6,18 +6,26 @@
             <?php
             foreach ($pictures as $picture) {
             ?>
-            <div class="col-ms-8 content_new_home">
-                <div>
-                    <figure class="col-ms-8">
-                        <img src="../Public/img/upload/avatar/avatar<?= $picture->getUser()->getId(); ?>.jpg" class="avatar_home" />
+            <div class="col-ms-8 content_new_post">
+                <div class="content_avatar_post">
+                    <figure>
+                        <img src="../Public/img/upload/avatar/avatar<?= $picture->getUser()->getId(); ?>.jpg" class="avatar_post" />
                     </figure>
-                    <p class="col-ms-4"><?= $picture->getUser()->getName(); ?></p>
+                    <p><?= $picture->getUser()->getName(); ?></p>
                 </div>
-
-                <figure>
-                    <img src="img/upload/picture/<?= $picture->getTitle() . $picture->getId(); ?>.jpg" class="picture_home" />
-                </figure>
-                <div>
+                <div class="content_picture_post">
+                    <figure class="picture_post">
+                        <a class="fancybox" rel="group" href="img/upload/picture/<?= $picture->getTitle() . $picture->getId(); ?>.jpg">
+                            <img src="img/upload/picture/<?= $picture->getTitle() . $picture->getId(); ?>.jpg" alt = "<?= $picture->getTitle(); ?>" class="picture_post"/>
+                        </a>
+                    </figure>
+                    <div class="content_icone_post">
+                        <span class="far fa-thumbs-up"></span>
+                        <span class="fas fa-flag"></span>
+                    </div>
+                </div>
+                <div class="content_info_post">
+                    <p>commentaires</p>
                     <p>Publiée le <?= date_format(date_create($picture->getUpload()), 'd/m/Y à H:i:s'); ?> </p>
                 </div>
             </div>
@@ -27,3 +35,4 @@
         </div>
     </div>
 </div>
+<script type = "text / javascript" src = "/fancybox/lib/jquery.mousewheel-3.0.6.pack.js" ></script>
