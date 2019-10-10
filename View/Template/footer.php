b5720a7abe6a4329b807c225442f347ce61fc931
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-10-10 06:28:24 +0200

    Modification footer.php

--- View/Template/footer.php
+++ View/Template/footer.php
@@ -1,8 +1,8 @@
         </div>
         <footer>
             <nav id="social_network">
-                <a href="https://www.facebook.com/sharer/sharer.php?u=http://projetformation.four.projetformationduhauvelle.fr/"><span class="fab fa-facebook-f" aria-hidden="true"></span></a>
-                <a href="https://twitter.com/intent/tweet/?url=http://projetformation.four.projetformationduhauvelle.fr/"><span class="fab fa-twitter" aria-hidden="true"></span></a>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=http://projetformation.five.projetformationduhauvelle.fr/"><span class="fab fa-facebook-f" aria-hidden="true"></span></a>
+                <a href="https://twitter.com/intent/tweet/?url=http://projetformation.five.projetformationduhauvelle.fr/"><span class="fab fa-twitter" aria-hidden="true"></span></a>
             </nav>
             <p>Copyright / Projet formation 2019</p>
         </footer>
