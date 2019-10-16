2cd1eccb7e667fe7c9eb3b5f6a1636411dcf54da
cedric-duhauvelle <duhauvelle.cedric@orange.fr>
2019-10-16 13:17:44 +0200

    Correction ajout.php

--- View/ajout.php
+++ View/ajout.php
@@ -6,7 +6,7 @@
         <form action="PictureController" method="POST" enctype="multipart/form-data" id="form_upload_picture">
             <label for="title_picture">Titre</label>
             <input type="text" name="title" id="title_picture" required />
-            <label for="upload_picture">Ajouter une photo: </label>
+            <label for="upload_picture_input">Ajouter une photo: </label>
             <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
             <input type="file" id="upload_picture_input" name="upload_picture" onchange="showMyImage(this)" />
             <input type="submit" class="btn btn-primary" id="button_upload_picture" title="Ajouter photo" />
